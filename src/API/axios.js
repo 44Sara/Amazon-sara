a02d60670c8aeae144759bda440e24ba718efa66
Jerusalem Hailu <hailus02@gmail.com>
2025-06-03 14:46:13 -0400

    done

--- src/API/axios.js
+++ src/API/axios.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // backend deployed on render
-  baseURL: "http://127.0.0.1:5001/clone-bf-36669/us-central1/api",
+  // baseURL: "http://127.0.0.1:5001/clone-bf-36669/us-central1/api",
   // backend locally using firebase functions
-  // baseURL: "http://127.0.0.1:5001/sep-449604/us-central1/api",
+  baseURL: "https://us-central1-clone-bf-36669.cloudfunctions.net/api",
   // backend locally using express server on port 5000
   // baseURL: "http://localhost:5000",
 });
